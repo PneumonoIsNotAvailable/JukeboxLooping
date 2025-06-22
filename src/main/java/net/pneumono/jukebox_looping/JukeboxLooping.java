c0a02699ea7041e6956fe3e47dd670d326eb8543
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-06-22 15:51:57 +0100

    Remove unused MOD_ID field

--- src/main/java/net/pneumono/jukebox_looping/JukeboxLooping.java
+++ src/main/java/net/pneumono/jukebox_looping/JukeboxLooping.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JukeboxLooping implements ModInitializer {
-	public static final String MOD_ID = "jukebox_looping";
 	public static final Logger LOGGER = LoggerFactory.getLogger("Jukebox Looping");
 
 	@Override
