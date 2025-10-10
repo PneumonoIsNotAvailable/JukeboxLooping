9d111b83b0684e4225d92edc1d97a8ceb61c6320
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-10-10 05:09:00 +0100

    Add Stonecutter

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+	id("fabric-loom") version "1.11-SNAPSHOT"
+	id("maven-publish")
+}
+
+val javaVersion = if (stonecutter.eval(stonecutter.current.version, ">=1.20.5"))
+	JavaVersion.VERSION_21 else JavaVersion.VERSION_17
+java.targetCompatibility = javaVersion
+java.sourceCompatibility = javaVersion
+
+base.archivesName = "${property("mod_id")}"
+version = "${property("mod_version")}+${stonecutter.current.project}+${property("mod_subversion")}"
+
+repositories {
+
+}
+
+dependencies {
+	minecraft("com.mojang:minecraft:${stonecutter.current.version}")
+	mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+	modImplementation("net.fabricmc:fabric-loader:0.16.14")
+
+	// Fabric API
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+}
+
+tasks {
+	processResources {
+		inputs.property("version", stonecutter.current.version)
+		inputs.property("min_supported", project.property("min_supported_version"))
+		inputs.property("max_supported", project.property("max_supported_version"))
+
+		filesMatching("fabric.mod.json") {
+			expand(
+				mutableMapOf(
+					"version" to stonecutter.current.version,
+					"min_supported" to project.property("min_supported_version"),
+					"max_supported" to project.property("max_supported_version")
+				)
+			)
+		}
+	}
+
+	withType<JavaCompile> {
+		val java = if (stonecutter.eval(stonecutter.current.version, ">=1.20.5")) 21 else 17
+		options.release.set(java)
+	}
+
+	java {
+		withSourcesJar()
+	}
+
+	jar {
+		from("LICENSE") {
+			rename { "${it}_${base.archivesName.get()}"}
+		}
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+		}
+	}
+
+	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+	repositories {
+		// Add repositories to publish to here.
+		// Notice: This block does NOT have the same function as the block in the top level.
+		// The repositories here will be used for publishing your artifact, not for
+		// retrieving dependencies.
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+	repositories {
+		maven("https://maven.fabricmc.net/")
+		mavenCentral()
+		gradlePluginPortal()
+		maven("https://maven.kikugie.dev/snapshots")
+	}
+}
+
+plugins {
+	id("dev.kikugie.stonecutter") version "0.7.10"
+}
+
+stonecutter {
+	centralScript = "build.gradle.kts"
+
+	create(rootProject) {
+		versions("1.20", "1.21")
+		vcsVersion = "1.21"
+	}
+}
