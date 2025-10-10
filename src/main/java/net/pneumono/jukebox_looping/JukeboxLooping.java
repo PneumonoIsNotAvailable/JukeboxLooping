a73c9380b4b0d099ce4d186f0f81cfcf45963bfe
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-10-10 05:29:23 +0100

    Add force_loop and block_loop tags

--- src/main/java/net/pneumono/jukebox_looping/JukeboxLooping.java
+++ src/main/java/net/pneumono/jukebox_looping/JukeboxLooping.java
@@ -2,20 +2,40 @@ package net.pneumono.jukebox_looping;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldAccess;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JukeboxLooping implements ModInitializer {
+	public static final String MOD_ID = "jukebox_looping";
 	public static final Logger LOGGER = LoggerFactory.getLogger("Jukebox Looping");
+
+	public static final TagKey<Block> FORCE_LOOP = TagKey.of(RegistryKeys.BLOCK, id("force_loop"));
+	public static final TagKey<Block> BLOCK_LOOP = TagKey.of(RegistryKeys.BLOCK, id("block_loop"));
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Jukebox Looping");
 	}
 
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+
 	public static boolean shouldJukeboxLoop(WorldAccess world, BlockPos pos) {
-		return world.getBlockEntity(pos.down()) == null;
+		BlockState state = world.getBlockState(pos);
+		if (state.isIn(FORCE_LOOP)) {
+			return true;
+		} else if (state.isIn(BLOCK_LOOP)) {
+			return false;
+		} else {
+			return world.getBlockEntity(pos.down()) == null;
+		}
 	}
 }
